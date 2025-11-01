e5708a3958396eb7ad14b8b7ab0bf8b89b06236a
hieurury <hieurury007@gmail.com>
2025-11-01 20:39:53 +0700

    thêm xử lý lỗi thủ công

--- BE/src/routers/index.js
+++ BE/src/routers/index.js
@@ -1,8 +1,17 @@
 import animalRouter from "./animal.route.js";
 
+//xử lý lỗi chung
+import { notFound, handleError } from "./error.js";
 
 const router = (App) => {
     App.use('/animals', animalRouter)
+
+
+    // ========== HANDLE ERROR ========== //
+    //lỗi không tìm thấy API
+    App.use(notFound);
+    //xử lý lỗi chung khi ứng dụng trả về lỗi
+    App.use(handleError);
 }
 
 export default router;
