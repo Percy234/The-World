c9d51f9e6eb7a3bdc68c4fc6cc8b7d4a08f322c1
hieurury <hieurury007@gmail.com>
2025-11-01 20:40:10 +0700

    thêm model KingDom (Giới)

---
+++ BE/src/models/KingDom.js
@@ -0,0 +1,9 @@
+import mongoose     from 'mongoose';
+const { Schema }    = mongoose;
+
+const KingDom = new Schema({
+
+})
+
+const model = mongoose.model('KingDom', KingDom);
+export default model;
